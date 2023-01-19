114be832e2baed777710d59da1c448963c28cc1a
hansung0904 <hansung0904@naver.com>
2023-01-19 17:03:35 +0900

    update : JavaStudy Day2 Exception

---
+++ JavaStudy/src/exception/ExceptionEx.java
@@ -0,0 +1,12 @@
+package exception;
+
+public class ExceptionEx {
+
+    public static void main(String[] args) {
+        try {
+
+        } catch (Exception e) {
+
+        } // try-catch 의 끝
+    } // main method 의 끝
+}
