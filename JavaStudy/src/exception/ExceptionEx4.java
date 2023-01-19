98ee3f9be123f31b92ade01975bcfe7e2ec8249d
hansung0904 <hansung0904@naver.com>
2023-01-19 17:45:59 +0900

    update : JavaStudy Day2 Exception Example

---
+++ JavaStudy/src/exception/ExceptionEx4.java
@@ -0,0 +1,16 @@
+package exception;
+
+public class ExceptionEx4 {
+
+    public static void main(String[] args) {
+
+        System.out.println(1);
+        System.out.println(2);
+        try {
+            System.out.println(3);
+            System.out.println(4);
+        } catch (Exception e) {
+            System.out.println(5); // -> 실행이 안됨.
+        }
+    }
+}
